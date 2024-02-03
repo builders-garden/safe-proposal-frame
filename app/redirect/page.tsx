2b4f57f326fd1833b0c86e39444c480d97b26b3a
lim0n3 <simonestaffa96@gmail.com>
2024-02-03 13:02:54 +0100

    fix safe url

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { redirect, useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 import { getSafeUrl } from '../../lib/safe';
 
@@ -9,23 +9,16 @@ export default function RedirectPage() {
 
   const address = searchParams.get('address');
 
-  console.log('address', address);
-
   if (!address) {
-    throw new Error('Missing address');
+    return (
+      <div>
+        <p>address query param not found</p>
+      </div>
+    );
   }
 
-  useEffect(() => {
-    console.log('here');
-    const safeUrl = getSafeUrl(address);
-
-    console.log('safeUrl', safeUrl);
-
-    // Perform the redirect
-    window.location.href = safeUrl; // For a full page reload redirect
-    // Or use Next.js router for client-side redirect (comment out the line above if using this)
-    // router.push(youtubeUrl);
-  }, [router]);
+  const safeUrl = getSafeUrl(address);
+  redirect(safeUrl);
 
   return (
     <div>
