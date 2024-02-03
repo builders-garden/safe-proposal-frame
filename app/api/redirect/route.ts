152500426d5ac9ff7b39b390cb9ec9e724ff1623
lim0n3 <simonestaffa96@gmail.com>
2024-02-03 13:36:07 +0100

    log base url

--- app/api/redirect/route.ts
+++ app/api/redirect/route.ts
@@ -4,6 +4,7 @@ import { BASE_URL } from '../../../lib/constants';
 export async function POST(req: NextRequest): Promise<Response> {
   const { searchParams } = new URL(req.url);
   const address = searchParams.get('address') ?? undefined;
+  console.log(`${BASE_URL}/redirect?address=${address}`);
   return new NextResponse(null, {
     status: 302,
     headers: { Location: `${BASE_URL}/redirect?address=${address}` },
